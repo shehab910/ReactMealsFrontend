e78bfa7f8745175fbbe6ade4f41d1e98c7659c09
Shehab <shehab@pop-os.localdomain>
2021-07-29 18:38:39 +0200

    11.6 added meals list and meals items

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,18 @@
+import styles from "./MealItem.module.css";
+
+const MealItem = (props) => {
+  const meal = props.meal;
+  const price = `$${meal.price.toFixed(2)}`;
+  return (
+    <li className={styles.meal}>
+      <div >
+        <h3>{meal.name}</h3>
+        <div className={styles.description}>{meal.description}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+      <div></div>
+    </li>
+  );
+};
+
+export default MealItem;
