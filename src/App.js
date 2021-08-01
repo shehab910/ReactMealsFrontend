26084c8df350e54e2c40f2ddeb33dc28f4d8e977
Shehab <shehab@pop-os.localdomain>
2021-08-01 21:56:21 +0200

    11.12 added the cart context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [isCartOn, setIsCartOn] = useState(false);
@@ -13,13 +14,13 @@ function App() {
     setIsCartOn(false);
   };
   return (
-    <>
+    <CartProvider>
       { isCartOn && <Cart onHideCart={hideCartHandler} /> }
       <main>
         <Header onShowCart={showCartHandler}/>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
-import CartIcon from "../Cart/CartIcon";
+import { useContext } from "react";
 
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
+
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
   return (
     <button onClick={props.onShowCart} className={styles.button}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numOfCartItems}</span>
     </button>
   );
 };
