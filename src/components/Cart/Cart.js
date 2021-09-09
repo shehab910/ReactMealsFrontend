46168d45261f3a720c1531a4060472cbf1831405
shehab <shehab@gmail.com>
2021-09-09 02:46:47 +0200

    11.16 added cart item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,31 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 import styles from "./Cart.module.css";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = () => {};
+
+  const cartItemRemoveHandler = () => {};
+
   const cartItems = (
     <ul className={styles["cart-items"]}>
-      {[
-        {
-          id: "m1",
-          name: "Sushi",
-          description: "Finest fish and veggies",
-          price: 22.99,
-        },
-      ].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onAdd={cartItemAddHandler}
+          onRemove={cartItemRemoveHandler} 
+        />
       ))}
     </ul>
   );
@@ -20,12 +33,23 @@ const Cart = (props) => {
     <Modal onShowCart={props.onShowCart} onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
-          <span>Total Amount: </span>
-          <span>22.99</span>
+        <span>Total Amount: </span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-          <button onClick={props.onHideCart} className={styles["button--alt"]}>Close</button>
-          <button onClick={() => {console.log("Ordering");}} className={styles.button}>Order</button>
+        <button onClick={props.onHideCart} className={styles["button--alt"]}>
+          Close
+        </button>
+        {hasItems && (
+          <button
+            onClick={() => {
+              console.log("Ordering");
+            }}
+            className={styles.button}
+          >
+            Order
+          </button>
+        )}
       </div>
     </Modal>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,16 +10,31 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
     //* here use concat() insted of push() to create new object better for state than modifing existing object
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
     return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
   }
-  if(action.type === 'REMOVE'){
-
+  if (action.type === "REMOVE") {
   }
   return defaultCartState;
 };
