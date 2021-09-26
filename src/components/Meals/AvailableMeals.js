019e981e5592ef8d175d4256962b67c22ac3dca1
shehab910 <shehabihab@hotmail.com>
2021-09-26 14:11:52 +0200

    17.5 meals data is now fetched from an api

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,37 +1,92 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+// const DUMMY_MEALS = [
+//   {
+//     id: "m1",
+//     name: "Sushi",
+//     description: "Finest fish and veggies",
+//     price: 22.99,
+//   },
+//   {
+//     id: "m2",
+//     name: "Schnitzel",
+//     description: "A german specialty!",
+//     price: 16.5,
+//   },
+//   {
+//     id: "m3",
+//     name: "Barbecue Burger",
+//     description: "American, raw, meaty",
+//     price: 12.99,
+//   },
+//   {
+//     id: "m4",
+//     name: "Green Bowl",
+//     description: "Healthy...and green...",
+//     price: 18.99,
+//   },
+// ];
+
+const apiLink =
+  "https://reactmeals-4dfb6-default-rtdb.europe-west1.firebasedatabase.app/meals.json";
+
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
-    <MealItem key={meal.id} meal={meal} />
-  ));
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(apiLink);
+      const data = await response.json();
+      if(!response.ok){
+        throw new Error('Something went wrong...');
+      }
+      const loadedMeals = [];
+      for (const key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          description: data[key].description,
+          price: data[key].price,
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  const mealsList = meals.map((meal) => <MealItem key={meal.id} meal={meal} />);
+  // meals.splice(0, meals.length); //simulating empty response
+  if (isLoading) {
+    return (
+      <section className={styles["meals-loading"]}>
+        <h2>Loading...</h2>
+      </section>
+    );
+  }
+  if(httpError){
+    return (
+      <section className={styles["meals-loading"]}>
+        <h2>{httpError}</h2>
+      </section>
+    );
+  }
+  if(meals.length === 0) {
+    return (
+      <section className={styles["meals-loading"]}>
+        <h2>No Meals Found.</h2>
+      </section>
+    );
+  }
   return (
     <section className={styles.meals}>
       <Card>
