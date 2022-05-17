e3582cfe62443d2134f5a605e14d7a04077f96ef
shehab910 <shehabihab@hotmail.com>
2022-05-17 05:40:55 +0200

    general cleanup, minor ui fix

--- src/store/cart-context.js
+++ src/store/cart-context.js
@@ -1,11 +1,11 @@
-import React from "react";
+import { createContext } from "react";
 
-const CartContext = React.createContext({
-    //* default data will not be used but will give auto-completion
-    items: [],
-    totalAmount: 0,
-    addItem: () => {},
-    removeItem: () =>{},
+const CartContext = createContext({
+   //* default data will not be used but will give auto-completion
+   items: [],
+   totalAmount: 0,
+   addItem: () => {},
+   removeItem: () => {},
 });
 
 export default CartContext;
